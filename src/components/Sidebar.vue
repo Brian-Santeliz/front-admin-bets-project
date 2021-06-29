7e6847960fb0e43dcf98105f20a07d8fc7d127e6
Brian <blsvsanteliz@gmail.com>
2021-06-29 18:44:45 -0400

    Create sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      class="blue darken-1 accent-3"
+      dark
+      v-model="drawer"
+      app
+      temporary
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="@/assets/img/admin.png"></v-img>
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Brian Santeliz</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn block class="red lighten-1"> Logout </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
+    <!-- <v-navigation-drawer v-model="drawer" app temporary dark>
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <img alt="Logo" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title class="title">Calango</v-list-item-title>
+            <v-list-item-subtitle>WEB</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider />
+
+      <v-list dense>
+        <v-list-item
+          v-for="([icon, text, link], i) in items"
+          :key="i"
+          link
+          @click="$vuetify.goTo(link)"
+        >
+          <v-list-item-icon class="justify-center">
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="subtitile-1">{{
+              text
+            }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+
+    <v-app-bar app color="primary" dark class="px-5">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Bienvenido Admin</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  data() {
+    return {
+      drawer: false,
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Account", icon: "mdi-account-box" },
+        { title: "Admin", icon: "mdi-gavel" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.v-toolbar {
+  transition: 0.6s;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    usuario:{
+      nombre:"",
+      id:""
+    },
+    token:null,
+    estaAutenticado:true
   },
   mutations: {
   },
